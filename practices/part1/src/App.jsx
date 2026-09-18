45e35891250b1ad19458b7aa72cdf10cf55b7da1
Tan Yi Jun <tanyijun6@gmail.com>
2026-09-18 17:48:01 +0800

    Add footer

--- practices/part1/src/App.jsx
+++ practices/part1/src/App.jsx
@@ -7,15 +7,20 @@ const Hello = (props) => {
   );
 }
 
-const App = () => {
-  const name = 'Peter';
-  const age = 10
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  );
+}
 
+const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Footer />
     </div>
   );
 }
